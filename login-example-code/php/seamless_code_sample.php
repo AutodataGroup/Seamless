23d72b73d0a5a77bf66bb3c1fc755d2ed11f70e9
Paweł Grądziel <pawel@gradziel.com>
2016-02-13 16:35:24 +0100

    update XML for sample decoded response

--- login-example-code/php/seamless_code_sample.php
+++ login-example-code/php/seamless_code_sample.php
@@ -141,7 +141,7 @@ $response_body_string = mcrypt_decrypt($encryption_method, $encryption_key, hex2
 //   <SessionToken>token</SessionToken>
 //   <RedirectUrl>http://seamlesslink</RedirectUrl>
 //   <EndUserId>GA2</EndUserId>
-// </Response$response_body_xml = new SimpleXMLElement($response_body_string);
+// </Response>
 
 echo "seamless link is: " . $response_body_xml->RedirectUrl;
 
